3e794db797a77c857ef205a06c97c086c37dd3bc
Sam6482 <20243ds141@utez.edu.mx>
2025-11-16 09:56:03 -0600

    Importando clases jua jua

--- app/src/main/java/com/example/miappgps/viewmodel/TrackingViewModel.kt
+++ app/src/main/java/com/example/miappgps/viewmodel/TrackingViewModel.kt
@@ -2,6 +2,12 @@ package com.example.miappgps.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.example.miappgps.data.Location.LocationClient
+import com.example.miappgps.data.repository.TripRepository
+import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 data class TrackingUiState(
     val isRecording: Boolean = false,
@@ -9,6 +15,19 @@ data class TrackingUiState(
 )
 
 class TrackingViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository = TripRepository(application)
+    private val locationClient = LocationClient(
+        application,
+        LocationServices.getFusedLocationProviderClient(application)
+    )
+
+    private val _uiState = MutableStateFlow(TrackingUiState())
+    val uiState = _uiState.asStateFlow()
+
+    private var locationJob: Job? = null
+
+
 
 
 }
